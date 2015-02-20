b732cfed508b3808a0f2cdd2c101322de0dd115c
Jacob Oscarson <jacob@plexical.com>
2015-02-20 13:21:28 +0100

    Proceed to Pleahub after the current user has signed out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SimpleCaptcha::ControllerHelpers
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  # See https://juntoo.co/s/10ca298
+  def after_sign_out_path_for(resource_or_scope)
+    "#{ENV['PLEATHUB_URL']}/"
+  end
 end
