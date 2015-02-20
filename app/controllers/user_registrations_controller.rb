fdd6c3b5c5225a19d2365c8d6a794ca9d1eeded7
Jacob Oscarson <jacob@plexical.com>
2015-02-20 16:44:32 +0100

    Redirect to Pleathub with a parameter if a user are newly registered but not verified

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,11 +1,19 @@
 class UserRegistrationsController < Devise::RegistrationsController
   def create
     if simple_captcha_valid?
-      super                   
-    else                         
-      build_resource         
+      super
+    else
+      build_resource
       resource.errors.add(:base, t('simple_captcha.error')) if resource.valid?
-      render :new                                 
-    end               
-  end    
+      render :new
+    end
+  end
+
+  protected
+
+  # See https://juntoo.co/s/3f9717c
+  # This controller was already started on.
+  def after_inactive_sign_up_path_for(resource)
+    "#{ENV['PLEATHUB_URL']}/?fresh=y"
+  end
 end
